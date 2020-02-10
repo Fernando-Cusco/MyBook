b67d9dbfbf7b6aeae359240e5387986c112634bb
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-02-10 06:20:51 -0500

    [fix] login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,6 +3,7 @@ import { UsuarioService } from '../../services/usuario.service';
 import { Usuario } from '../register/usuario';
 import { Router } from '@angular/router';
 import { CarritoService } from '../../services/carrito.service';
+import { ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -14,21 +15,44 @@ export class LoginPage implements OnInit {
   idUser: number;
   usuario: Usuario = new Usuario();
 
-  constructor(private service: UsuarioService, private router: Router, private online: CarritoService) { }
+  constructor(private service: UsuarioService, private router: Router, private online: CarritoService, private toast: ToastController, private loading: LoadingController) { }
 
   ngOnInit() {
   }
 
   login() {
     this.service.login(this.usuario).subscribe(response => {
-      console.log(response, 'aqui');
-      
       this.idUser = response.id;
-      this.online.usuarioOnline(this.idUser);
-      this.router.navigate(['/inicio'], {queryParams: {
-        id: this.idUser
-      }});
+      if(this.idUser > 0) {
+        this.online.usuarioOnline(this.idUser);
+        this.router.navigate(['/inicio'], {queryParams: {
+          id: this.idUser
+        }});
+      } else if( this.idUser == 0) {
+        this.mensaje('Datos Incorrecto');
+      }
     });
+  }
+
+  async mensaje(msj: string) {
+    const toast = await this.toast.create({
+      message: msj,
+      duration: 1500
+    });
+    toast.present();
+  }
+
+  async iniciar() {
+    const l = await this.loading.create({
+      message: 'Espera...',
+      duration: 1500,
+      translucent: true
+    });
+    l.present();
+    this.login()
+
+    const { role, data } = await l.onDidDismiss();
+    
   }
 
 
