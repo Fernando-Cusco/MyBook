4531ec8145383b4409df35e93cec62e902b1df1c
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-02-10 12:38:28 -0500

    [fix] ip servidor

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -10,42 +10,42 @@ import { Direccion } from '../pages/register/direccion';
   providedIn: 'root'
 })
 export class UsuarioService {
-  private url:string = 'http://http://34.95.200.214/Libreria/rest/usuario'
+  private url:string = 'http://34.95.200.214/Libreria/faces/rest/usuario'
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
   constructor(private http: HttpClient) { }
 
   login(usuario: Usuario):Observable<Usuario> {
-    return this.http.post<Usuario>('http://http://34.95.200.214/Libreria/rest/usuario/login', usuario, {headers: this.httpHeaders});
+    return this.http.post<Usuario>('http://34.95.200.214/Libreria/faces/rest/usuario/login', usuario, {headers: this.httpHeaders});
   }
 
 
   registro(usuario: Usuario): Observable<Respuesta> {
     
-    return this.http.post<Respuesta>('http://http://34.95.200.214/Libreria/rest/usuario/registro', usuario, {headers: this.httpHeaders});
+    return this.http.post<Respuesta>('http://34.95.200.214/Libreria/faces/rest/usuario/registro', usuario, {headers: this.httpHeaders});
   }
 
   direcciones(id: number): Observable<Direccion[]>{
-    return this.http.get<Direccion[]>(`http://http://34.95.200.214/Libreria/rest/compras/direcciones?id=${id}`);
+    return this.http.get<Direccion[]>(`http://34.95.200.214/Libreria/faces/rest/compras/direcciones?id=${id}`);
 
   }
 
   tarjetas(id: number): Observable<Tarjeta[]>{
-    return this.http.get<Tarjeta[]>(`http://http://34.95.200.214/Libreria/rest/compras/tarjetas?id=${id}`);
+    return this.http.get<Tarjeta[]>(`http://34.95.200.214/Libreria/faces/rest/compras/tarjetas?id=${id}`);
 
   }
 
   agregarTarjeta(tarjeta: Tarjeta): Observable<Respuesta> {
     console.log('Enviando '+tarjeta);
     
-    return this.http.post<Respuesta>(`http://http://34.95.200.214/Libreria/rest/compras/agregarTarjeta`, tarjeta, {headers: this.httpHeaders});
+    return this.http.post<Respuesta>(`http://34.95.200.214/Libreria/faces/rest/compras/agregarTarjeta`, tarjeta, {headers: this.httpHeaders});
   }
 
   listarUsuarios(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>('http://http://34.95.200.214/Libreria/rest/usuario/usuarios');
+    return this.http.get<Usuario[]>('http://34.95.200.214/Libreria/faces/rest/usuario/usuarios');
   }
 
   misCompras(id: number):Observable<any[]> {
-    return this.http.get<any[]>(`http://http://34.95.200.214/Libreria/rest/usuario/miscompras?id=${id}`);
+    return this.http.get<any[]>(`http://34.95.200.214/Libreria/faces/rest/usuario/miscompras?id=${id}`);
   }
 
 }
